50891f3303c59b200ae94e59c9cd7c0cc9cee3b6
X0080-闫文清 <yanwenqing@tecorigin.com>
2024-03-21 18:33:55 +0800

    produce server

---
+++ tecmq/digital_produce_service.go
@@ -0,0 +1,40 @@
+package tecmq
+
+import (
+	"encoding/json"
+	"time"
+)
+
+const (
+	DigitalProduceSubject = "digital.produce"
+)
+
+type DigitalProduceMsg struct {
+	Source           uint8                  `json:"source"` // 服务来源类型 1-事件监控系统 2-监控告警系统 3-....
+	Data             *DigitalProduceMsgData // 发送消息信息
+	SourceCreateTime time.Time              // 消息创建时间
+}
+
+// DigitalProduceMsgData 订阅主题使用的消息
+type DigitalProduceMsgData struct {
+	BackgroundUri       string `json:"background_uri"`
+	FaceVideo           string `json:"face_video"`
+	AudionUri           string `json:"audion_uri"`
+	StoryboardId        int32  `json:"storyboard_id"`
+	StoryboardSectionId int32  `json:"storyboard_section_id"`
+	Payload             string `json:"payload"`
+}
+
+// PushProduceMessage to ProduceServer 发送生产视频的消息
+func (n *NatJs) PushProduceMessage(subject string, msgD *DigitalProduceMsg) error {
+	b, err := json.Marshal(msgD)
+	if err != nil {
+		return err
+	}
+	err = n.JsPushMessage(subject, b)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
